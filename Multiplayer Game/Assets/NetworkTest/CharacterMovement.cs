d9d04e1f1fc8de2c902dba21106972570b887c3a
nitrogendragon <dirtboy4fun@gmail.com>
2018-09-27 12:24:22 -0500

    better multiplayer in progress

---
+++ Multiplayer Game/Assets/NetworkTest/CharacterMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+            var x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+        
+    //    if (characterController.isGrounded)
+    //    {
+    //        // We are grounded, so recalculate
+    //        // move direction directly from axes
+
+    //        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+    //        moveDirection *= speed;
+
+    //        if (Input.GetButton("Jump"))
+    //        {
+    //            moveDirection.y = jumpSpeed;
+    //        }
+    //    }
+
+    //    // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+    //    // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+    //    // as an acceleration (ms^-2)
+    //    moveDirection.y -= gravity * Time.deltaTime;
+
+    //    // Move the controller
+    //    characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Multiplayer Game/Assets/NetworkTest/setuplocalplayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class setuplocalplayer : NetworkBehaviour {
+    [SyncVar]
+    public string pname = "player";
+
+    void OnGUI()
+    {
+        if (isLocalPlayer)
+        {
+            pname = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pname);
+            if(GUI.Button(new Rect(130,Screen.height - 40, 80, 30), "Change"))
+            {
+                CmdChangeName(pname);
+            }
+        }
+    }
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pname = newName;
+    }
+    // Use this for initialization
+    void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<CharacterMovement>().enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (isLocalPlayer)
+            this.GetComponentInChildren<TextMesh>().text = pname;
+        	
+	}
+}
