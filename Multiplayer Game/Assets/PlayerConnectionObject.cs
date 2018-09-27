3fde5eac72dfbff5199b5776c43baf0124a130da
nitrogendragon <dirtboy4fun@gmail.com>
2018-09-26 22:34:28 -0500

    so many things did not go well today

--- Multiplayer Game/Assets/PlayerConnectionObject.cs
+++ Multiplayer Game/Assets/PlayerConnectionObject.cs
@@ -7,6 +7,7 @@ public class PlayerConnectionObject : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        playerCamera.enabled = false;
         //is this actually my player?
         if(isLocalPlayer == false)
         {
@@ -24,7 +25,11 @@ public class PlayerConnectionObject : NetworkBehaviour {
 
 	}
     public GameObject PlayerUnitPrefab;//our physical player
+    public Camera playerCamera;
 
+    //SyncVars are variables where if their value changes on the sERVER, then all clients
+    //are automatically informed of the new value.
+    [SyncVar(hook="OnPlayerNameChanged") ]
     public string PlayerName = "Anonymous";
 
 
@@ -43,7 +48,7 @@ public class PlayerConnectionObject : NetworkBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            string n = "Quill" + Random.Range(1, 100);
+            string n = "Dude" + Random.Range(1, 100);
             Debug.Log("sending the server a msg to change the players name to " + n);
             CmdChangePlayerName(n);
         }
@@ -54,27 +59,51 @@ public class PlayerConnectionObject : NetworkBehaviour {
 
 
     }
-
+    //Warning: if you use a hook on a SyncVar, then our local value does not get automatically udated
+    void OnPlayerNameChanged(string newName)
+    {
+        Debug.Log("OnPlayerNameChanged: OldName:  " + PlayerName+ "NewName: " + newName);
+        PlayerName = newName;
+        gameObject.name = "PLayerConnectionObject [" + newName + "]";
+    }
     //GameObject myPlayerUnit;
-
+    public GameObject go;
     /////////COMMANDS
     //commands are special functions that Only get executed ont he server.
     [Command]//executes on the server
     void CmdSpawnMyUnit()//must start with Cmd to be a Command function
     {
+        if(go)//dont want copies of me causing problems
+        {
+            Destroy(go);
+            //DestroyImmediate(Camera.main.gameObject);
+            
+        }
         //We are guarnateed to be on the server right now.
-        GameObject go = Instantiate(PlayerUnitPrefab);//give me something to see that I use
-
+        go = Instantiate(PlayerUnitPrefab);//give me something to see that I use
+        if (playerCamera.enabled)
+        {
+            return;
+            Debug.Log("shouldn't be summoning");
+        }
+        else
+        {
+            playerCamera.enabled = true;
+            Instantiate(playerCamera);
+            playerCamera = Camera.main;
+        }
+        //Camera.main.enabled = true;
         //go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
         //connectiontoclient points to person that has this player object
 
-       // myPlayerUnit = go;
+        // myPlayerUnit = go;
 
 
 
         //now that the object exists on the server, propagate it to all the clients
         //also wire up the NetworkIdentity
         NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+        
     }
 
     [Command]
@@ -82,8 +111,21 @@ public class PlayerConnectionObject : NetworkBehaviour {
     {
         Debug.Log("CmdChangePlayerName: " + n);
         PlayerName = n;
+        //tell all the clients what this players name now is
+        //RpcChangePlayerName(PlayerName);
     }
+    ////////////////////RPC
+    // RPC's are special functions that ONLY get executed on the clients
 
+   /* [ClientRpc]
+    void RpcChangePlayerName(string n)
+    {
+        Debug.Log("RpcChangePlayerName: we were asked to change the player name on a particualr PlayerConnectionObject: " + n);
+        PlayerName = n;
+
+       
+    }
+    */
 
     //[Command]
     //void CmdMoveUnitUp()
--- Multiplayer Game/Assets/PlayerUnit.cs
+++ Multiplayer Game/Assets/PlayerUnit.cs
@@ -8,17 +8,37 @@ using UnityEngine.Networking;
 
 public class PlayerUnit : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+   
+    // Use this for initialization
+    void Start()
+    {
+
+        
+    }
+
+
+
+    float speed=50;
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        gameObject.GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update () {
+       
         //this function runs on ALL PlayerUnits -- not just the ones that I own.
         //How do I verify that I am allowed to mess with this object?
-       // if (isLocalPlayer == false)
-        if(hasAuthority == false)
+        // if (isLocalPlayer == false)
+        if (hasAuthority == false)
         {
             return;
         }
@@ -32,6 +52,25 @@ public class PlayerUnit : NetworkBehaviour {
         if (Input.GetKeyDown(KeyCode.D))
         {
             Destroy(gameObject);
+            Debug.Log("destroyed");
+            //DestroyImmediate(Camera.main.gameObject);
+           // Camera.main.enabled = false;
         }
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    gameObject.GetComponent<Rigidbody>().AddRelativeForce(0, 0, thrust, ForceMode.Acceleration);
+        //}
+        //if (Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    gameObject.GetComponent<Rigidbody>().AddRelativeForce(0, 0, -thrust, ForceMode.Acceleration);
+        //}
+        //if (Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    gameObject.GetComponent<Rigidbody>().AddRelativeForce(-thrust, 0, 0, ForceMode.Acceleration);
+        //}
+        //if (Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    gameObject.GetComponent<Rigidbody>().AddRelativeForce(thrust, 0, 0, ForceMode.Acceleration);
+        //}
     }
 }
