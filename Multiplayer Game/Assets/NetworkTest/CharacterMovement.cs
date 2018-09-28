24d59c3b7fc42d5127714603b444814e6599be78
nitrogendragon <dirtboy4fun@gmail.com>
2018-09-27 20:16:44 -0500

    everything is working in this version

--- Multiplayer Game/Assets/NetworkTest/CharacterMovement.cs
+++ Multiplayer Game/Assets/NetworkTest/CharacterMovement.cs
@@ -2,16 +2,12 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
-// This script moves the character controller forward
-// and sideways based on the arrow keys.
-// It also jumps when pressing space.
-// Make sure to attach a character controller to the same game object.
-// It is recommended that you make only one call to Move or SimpleMove per frame.
+
 
 public class CharacterMovement : MonoBehaviour
 {
-    CharacterController characterController;
-
+    
+    Animator anim;
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
@@ -20,7 +16,7 @@ public class CharacterMovement : MonoBehaviour
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        anim = this.GetComponent<Animator>();
     }
 
     void Update()
@@ -31,27 +27,25 @@ public class CharacterMovement : MonoBehaviour
 
             transform.Rotate(0, x, 0);
             transform.Translate(0, 0, z);
-        
-    //    if (characterController.isGrounded)
-    //    {
-    //        // We are grounded, so recalculate
-    //        // move direction directly from axes
+        if (z > 0)
+        {
+            
+            anim.SetBool("running", true);
+        }
+        else if (z < 0)
+        {
 
-    //        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-    //        moveDirection *= speed;
+            
+            anim.SetBool("backstepping", true);
 
-    //        if (Input.GetButton("Jump"))
-    //        {
-    //            moveDirection.y = jumpSpeed;
-    //        }
-    //    }
-
-    //    // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-    //    // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-    //    // as an acceleration (ms^-2)
-    //    moveDirection.y -= gravity * Time.deltaTime;
-
-    //    // Move the controller
-    //    characterController.Move(moveDirection * Time.deltaTime);
+        }
+        else
+        {
+            
+            anim.SetBool("running", false);
+            anim.SetBool("backstepping", false);
+        }
     }
+   
+    
 }
