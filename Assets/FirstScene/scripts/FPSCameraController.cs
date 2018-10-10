2af1b101ff2617bc6f69b73244ee0cb58ee7259c
Amber Frazier <afrazier20@cornellcollege.edu>
2018-10-10 09:59:03 -0500

    updated size

---
+++ Assets/FirstScene/scripts/FPSCameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FPSCameraController : MonoBehaviour {
+    public GameObject player;
+    public int rightlimit = 60;
+    public int leftlimit = -60;
+    public int vertuplimit = 45;
+    public int vertlowlimit = -25;
+    int horval;
+    int vertval;
+    public int horspeed =1;
+    public int vertspeed =1;
+    Vector3 initialrotation;
+    Vector3 initcamrot;
+    public int vertadjust;
+
+    private void Start()
+    {
+        vertadjust = 20;
+    }
+    private void Update()
+    {
+        initialrotation = player.transform.eulerAngles;
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            
+                this.transform.localEulerAngles = new Vector3(20, 0, 0);
+                horval = 0;
+                vertval = 0;
+            
+        }
+        if (Input.GetKey(KeyCode.J)&& horval>leftlimit)
+        {
+            transform.localEulerAngles += new Vector3(0, -horspeed, 0);
+            horval += -horspeed;
+        }
+        if (Input.GetKey(KeyCode.L) && horval < rightlimit)
+        {
+            transform.localEulerAngles += new Vector3(0, horspeed, 0);
+            horval += horspeed;
+        }
+        if (Input.GetKey(KeyCode.I)&& vertval < vertuplimit)
+        {
+            transform.localEulerAngles += new Vector3(-vertspeed, 0, 0);
+            vertval += vertspeed;
+        }
+        if (Input.GetKey(KeyCode.K) && vertval >vertlowlimit)
+        {
+            transform.localEulerAngles += new Vector3(vertspeed, 0, 0);
+            vertval += -vertspeed;
+        }
+    }
+}
+
+
+
+
---
+++ Assets/FirstScene/scripts/attachplayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class attachplayer : NetworkBehaviour {
+    
+    
+    updatecubeposition ucp;
+    private void Start()
+    {
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = transform;
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            other.gameObject.GetComponent<CharacterMovement>().wasattached = true;
+            other.gameObject.GetComponent<CharacterMovement>().zairspeed = this.GetComponent<updatecubeposition>().currentspeed.z;
+            other.gameObject.GetComponent<CharacterMovement>().xairspeed = this.GetComponent<updatecubeposition>().currentspeed.x;
+            other.gameObject.GetComponent<CharacterMovement>().yairspeed = this.GetComponent<updatecubeposition>().currentspeed.y;
+            other.gameObject.transform.parent = null;
+
+
+        }
+    }
+}
